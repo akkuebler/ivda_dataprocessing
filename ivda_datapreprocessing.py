e653971212f15c7f65fb5ca03578574c22f253b5
akkuebler <91547040+akkuebler@users.noreply.github.com>
2022-08-17 14:31:50 +0200

    prep data to use for students

--- ivda_datapreprocessing.py
+++ ivda_datapreprocessing.py
@@ -6,15 +6,76 @@ import _pickle as cp
 # Task 1.1
 # Come up with a strategy to have a good dataset consisting of 5000 rows
 
-# TODO AK: build messy dataset with 5000 + rows
+# TODO AK: build messy dataset with 5000 + rows (3000 clean, 2000 missing, rest total mess) 
+# pandas filter 
+# check them for duplicates
 # dataset prep to work with
 df = pd.read_csv (r'/Users/anki/Documents/Uni_Zurich/IVDA_teaching/covid_impact.csv', sep=";")
 print (df[:10])
 
-df_shuffled = df.sample(frac=1, random_state=22)
-print(df_shuffled[:10])
+#check for missing data we find a lot 
+df.columns[df.isnull().any()]
 
-df_work=df_shuffled[:1000]
+
+#filter data which has missing values in Q1 - Q9, Q25, Q17
+# TODO AK: can we make it nicer and loop through it? 
+#for x in ("Q1"):
+ #   df=df[df[x].notnull()]
+
+df_clean = df
+df_clean = df_clean[df_clean["Q1"].notnull()]
+df_clean = df_clean[df_clean["Q3"].notnull()]
+df_clean = df_clean[df_clean["Q4"].notnull()]
+df_clean = df_clean[df_clean["Q5"].notnull()]
+df_clean = df_clean[df_clean["Q6"].notnull()]
+df_clean = df_clean[df_clean["Q7"].notnull()]
+df_clean = df_clean[df_clean["Q8"].notnull()]
+df_clean = df_clean[df_clean["Q9"].notnull()]
+df_clean = df_clean[df_clean["Q17"].notnull()]
+df_clean = df_clean[df_clean["Q25a"].notnull()]
+df_clean = df_clean[df_clean["Q25b"].notnull()]
+df_clean = df_clean[df_clean["Q25c"].notnull()]
+df_clean = df_clean[df_clean["Q25d"].notnull()]
+df_clean = df_clean[df_clean["Q25e"].notnull()]
+df_clean = df_clean[df_clean["Q25f"].notnull()]
+df_clean = df_clean[df_clean["Q25g"].notnull()]
+df_clean = df_clean[df_clean["Q25h"].notnull()]
+df_clean = df_clean[df_clean["Q25i"].notnull()]
+df_clean = df_clean[df_clean["Q25j"].notnull()]
+# 11237 rows left
+
+#shuffle data and reduce to 3000 rows
+df_shuffled = df_clean.sample(frac=1, random_state=222)
+print(df_shuffled.shape)
+
+df_clean=df_shuffled[:3000]
+
+# select 2000 rows with a few missing data
+# TODO AK: but only checked specific row for missing data
+df_missing_Q1 = (df[df["Q1"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q3 = (df[df["Q3"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q4 = (df[df["Q4"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q5 = (df[df["Q5"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q6 = (df[df["Q6"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q7 = (df[df["Q7"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q8 = (df[df["Q8"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q9 = (df[df["Q9"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q17 = (df[df["Q17"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25a = (df[df["Q25a"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25b = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25c = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25d = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25e = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25f = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25g = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25h = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25i = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+df_missing_Q25j = (df[df["Q25b"].isnull()]).sample(frac=1, random_state=222)[:100]
+
+
+# put them together
+frames = [df_missing_Q1, df_missing_Q3, df_missing_Q4]
+result = pd.concat(frames)
 
 
 
@@ -27,7 +88,7 @@ data_top
 
 # check the size of the data
 print("The dataset contains {} data records and {} features.".format(df_work.shape[0], df_work.shape[1]))
-
+# TODO AK: more if theese if possible with blank behind
 
 # show the first 10 rows: 
 print (df_work[:10])
@@ -43,7 +104,7 @@ def counting_as_dict(c):
     countries = {}
     for country in c:
         #country = country.lower() not possible with none value
-        # TODO: do we want to include spelling mistakes and lower/upper case issues?
+        # TODO: do we want to include spelling mistakes and lower/upper case issues? nope -- string parsing? 
         countries[country] = countries.get(country, 0) + 1
     return countries
 
@@ -75,6 +136,9 @@ plot_pie(counting_as_dict(c))
 # TODO AK: provide continent table
 
 
+# filling a null values using fillna()
+#data["Gender"].fillna("No Gender", inplace = True)
+
 
 
 
@@ -96,7 +160,7 @@ plt.show()
 
 # Task 3.2 (cleaned): Visualize again the age of the students in your dataset with an appropriate boxplot.
 # This time do not ignore missing values, but think about how you can treat them. Explain your decision. 
-# Take also into account the context your working with.  
+# Take also into account the context your working with. --> 3 options
 
 
 
